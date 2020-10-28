0f66c08318b0b78be7b58116d52ee698956169d6
Ben Jones <benwjones76@outlook.com>
2020-10-28 14:53:40 +0000

    added a web_helper to DRY up the code

--- spec/features/hit_point_spec.rb
+++ spec/features/hit_point_spec.rb
@@ -1,9 +1,7 @@
 feature 'display hit points' do
   scenario "show player 2's hit points" do
-    visit('/')
-    fill_in 'player1', with: 'Ben'
-    fill_in 'player2', with: 'Bhups'
-    click_button "Submit"
+    sign_in_and_play()
     expect(page).to have_content "Bhups: 60HP"
   end
+
 end
--- spec/features/player_names_spec.rb
+++ spec/features/player_names_spec.rb
@@ -1,9 +1,6 @@
 feature "Player Names" do
   scenario "Players enter their names and they are displayed" do
-    visit('/')
-    fill_in 'player1', with: 'Ben'
-    fill_in 'player2', with: 'Bhups'
-    click_button "Submit"
+    sign_in_and_play()
     expect(page).to have_content 'Ben vs. Bhups'
   end
 end
